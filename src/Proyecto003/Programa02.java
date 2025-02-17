117f301c00e202eb7fc76be1943b3ed70bab3bf5
JC_dev <juancarlostorresmiranda96@gmail.com>
2025-02-17 00:37:27 -0500

    Carga inicial

---
+++ src/Proyecto003/Programa02.java
@@ -0,0 +1,30 @@
+package Proyecto003;
+import java.util.Scanner;
+public class Programa02 {
+      public static void main( String args[]){
+          
+        String empleado;
+        double ht,th,sb,d,sn;
+        Scanner lee=new Scanner(System.in);
+ 
+        System.out.print("Nombre de empleado: ");
+        empleado=lee.next();
+        
+        System.out.print("Horas trabajadas:");
+        ht=lee.nextDouble();
+        
+        System.out.print("Tarifa por hora: ");
+        th=lee.nextDouble();
+        
+        sb=ht*th;
+        d=sb*0.13;
+        sn=sb-d;
+      
+        System.out.println("Sueldo bruto: s/" + sb);
+        System.out.println("Descuento: " + d);
+        System.out.println("Sueldo neto: " + sn);
+    }
+}
+
+    
+    
